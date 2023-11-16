36b710cfd6230a64685556b37d72107a9e7416e2
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-11-16 11:13:04 +0300

    Settings loading

--- app/application.py
+++ app/application.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
 from .containers import Container
+from .config import Settings
 
 
 def create_app() -> Flask:
     container = Container()
+    container.config.from_pydantic(Settings())
     app = Flask(__name__)
     app.container = container
     return app
