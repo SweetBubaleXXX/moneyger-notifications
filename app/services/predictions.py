9c7fa3a890ad081f3dbf37b77b161b180ac1824d
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-12-05 09:59:13 +0300

    Add predictions service

---
+++ app/services/predictions.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+from decimal import Decimal
+
+import pandas as pd
+import pmdarima
+
+from .transactions import TransactionsService
+
+
+class PredictionsService:
+    def __init__(self, transactions_service: TransactionsService) -> None:
+        self.transactions_service = transactions_service
+
+    def predict_week(self, account_id: int) -> Decimal:
+        daily_totals = self._get_totals(account_id, days=365)
+        return self._make_prediction(daily_totals, "W-MON")
+
+    def predict_month(self, account_id: int) -> Decimal:
+        daily_totals = self._get_totals(account_id, days=730)
+        return self._make_prediction(daily_totals, "M")
+
+    def _get_totals(self, account_id: int, days: int) -> pd.DataFrame:
+        daily_totals = self.transactions_service.compute_daily_total(
+            account_id,
+            start_time=datetime.now() - timedelta(days=days),
+        )
+        dataset = pd.DataFrame(daily_totals)
+        dataset["date"] = pd.to_datetime(dataset["date"])
+        dataset.set_index("date", inplace=True)
+        return dataset
+
+    def _make_prediction(self, dataset: pd.DataFrame, resample_rule: str) -> Decimal:
+        time_series = dataset["total_amount"].resample(resample_rule).sum().fillna(0)
+        model = pmdarima.auto_arima(time_series)
+        predictions = model.predict(1)
+        return round(Decimal(predictions.iloc[0]), 2)
